987ac56a49252dc5fabc4d17098f295b417b69b4
kaspas <panagiwtis.kas@gmail.com>
2020-07-05 22:08:59 +0300

    Add files via upload

---
+++ composer/php/includes/app.definitions.php
@@ -0,0 +1,14 @@
+<?php
+
+// Credential File link
+define("CREDENTIALS_FILE", "data/credentials.json");
+
+// Folder Paths
+define("Gallery_Folder", "covid-chestxray-dataset/");
+
+/* Page Links */
+define("Gallery_Page_Link" , "gallery.php");
+define("Index_Page_Link" , "index.php");
+define("Photos_Page_Link" , "photos.php");
+
+?>
